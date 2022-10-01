bc2f40dce712a8c3b5ff8d55cbfc52e22b28cea1
adrifcastr <adrifcastr@gmail.com>
2022-10-01 18:07:48 +0200

    lint

--- src/fetch.rs
+++ src/fetch.rs
@@ -1,6 +1,6 @@
-use graphql_client::{GraphQLQuery};
-use std::error::Error;
+use graphql_client::GraphQLQuery;
 use poll_promise::Promise;
+use std::error::Error;
 
 #[derive(GraphQLQuery)]
 #[graphql(
@@ -18,58 +18,68 @@ pub struct NumQuery;
 )]
 pub struct NameQuery;
 
-pub fn fetch_dex_num(num: num_query::Variables) -> Result<graphql_client::Response<num_query::ResponseData>, Box<dyn Error>> {
-	let mut promise: Option<Promise<Vec<u8>>> = None;
+pub fn fetch_dex_num(
+    num: num_query::Variables,
+) -> Result<graphql_client::Response<num_query::ResponseData>, Box<dyn Error>> {
+    let mut promise: Option<Promise<Vec<u8>>> = None;
 
-	let result = promise.get_or_insert_with(|| {
-		let (sender, promise) = Promise::new();
-		let request_body = NumQuery::build_query(num);
-		let request_body = serde_json::to_vec(&request_body);
+    let result = promise.get_or_insert_with(|| {
+        let (sender, promise) = Promise::new();
+        let request_body = NumQuery::build_query(num);
+        let request_body = serde_json::to_vec(&request_body);
 
-		let request = ehttp::Request {
-			headers: ehttp::headers(&[
-				("Accept", "*/*"),
-				("Content-Type", "application/json"),
-			]),
-			..ehttp::Request::post("https://graphqlpokemon.favware.tech/v7", request_body.unwrap())
-		};
+        let request = ehttp::Request {
+            headers: ehttp::headers(&[("Accept", "*/*"), ("Content-Type", "application/json")]),
+            ..ehttp::Request::post(
+                "https://graphqlpokemon.favware.tech/v7",
+                request_body.unwrap(),
+            )
+        };
 
-		ehttp::fetch(request, move |response| {
-			let bytes = response.unwrap().bytes.to_vec();
-			sender.send(bytes);
-		});
-		promise
-	});
-	
-	let response_bytes = result.block_until_ready().clone();
-	let body = serde_json::from_slice::<graphql_client::Response<num_query::ResponseData>>(&response_bytes).unwrap();
-	Ok(body)
+        ehttp::fetch(request, move |response| {
+            let bytes = response.unwrap().bytes.to_vec();
+            sender.send(bytes);
+        });
+        promise
+    });
+
+    let response_bytes = result.block_until_ready().clone();
+    let body = serde_json::from_slice::<graphql_client::Response<num_query::ResponseData>>(
+        &response_bytes,
+    )
+    .unwrap();
+    Ok(body)
 }
 
-pub fn fetch_dex_name(name: name_query::Variables) -> Result<graphql_client::Response<name_query::ResponseData>, Box<dyn Error>> {
-	let mut promise: Option<Promise<Vec<u8>>> = None;
+pub fn fetch_dex_name(
+    name: name_query::Variables,
+) -> Result<graphql_client::Response<name_query::ResponseData>, Box<dyn Error>> {
+    let mut promise: Option<Promise<Vec<u8>>> = None;
 
-	let result = promise.get_or_insert_with(|| {
-		let (sender, promise) = Promise::new();
-		let request_body = NameQuery::build_query(name);
-		let request_body = serde_json::to_vec(&request_body);
+    let result = promise.get_or_insert_with(|| {
+        let (sender, promise) = Promise::new();
+        let request_body = NameQuery::build_query(name);
+        let request_body = serde_json::to_vec(&request_body);
 
-		let request = ehttp::Request {
-			headers: ehttp::headers(&[
-				("Accept", "*/*"),
-				("Content-Type", "application/json"),
-			]),
-			..ehttp::Request::post("https://graphqlpokemon.favware.tech/v7", request_body.unwrap())
-		};
+        let request = ehttp::Request {
+            headers: ehttp::headers(&[("Accept", "*/*"), ("Content-Type", "application/json")]),
+            ..ehttp::Request::post(
+                "https://graphqlpokemon.favware.tech/v7",
+                request_body.unwrap(),
+            )
+        };
 
-		ehttp::fetch(request, move |response| {
-			let bytes = response.unwrap().bytes.to_vec();
-			sender.send(bytes);
-		});
-		promise
-	});
-	
-	let response_bytes = result.block_until_ready().clone();
-	let body = serde_json::from_slice::<graphql_client::Response<name_query::ResponseData>>(&response_bytes).unwrap();
-	Ok(body)
+        ehttp::fetch(request, move |response| {
+            let bytes = response.unwrap().bytes.to_vec();
+            sender.send(bytes);
+        });
+        promise
+    });
+
+    let response_bytes = result.block_until_ready().clone();
+    let body = serde_json::from_slice::<graphql_client::Response<name_query::ResponseData>>(
+        &response_bytes,
+    )
+    .unwrap();
+    Ok(body)
 }
--- src/images.rs
+++ src/images.rs
@@ -1,37 +1,37 @@
-use std::error::Error;
 use poll_promise::Promise;
+use std::error::Error;
 use viuer::{print, Config};
 
 fn fetch_image(url: &str) -> Result<image::DynamicImage, Box<dyn Error>> {
-	let mut promise: Option<Promise<image::DynamicImage>> = None;
+    let mut promise: Option<Promise<image::DynamicImage>> = None;
 
-	let result = promise.get_or_insert_with(|| {
-		let (sender, promise) = Promise::new();
-		let request = ehttp::Request::get(url);
-		ehttp::fetch(request, move |response| {
-			let image = image::load_from_memory(&response.unwrap().bytes).unwrap();
-			sender.send(image);
-		});
-		promise
-	});
-	
-	Ok(result.block_until_ready().clone())
+    let result = promise.get_or_insert_with(|| {
+        let (sender, promise) = Promise::new();
+        let request = ehttp::Request::get(url);
+        ehttp::fetch(request, move |response| {
+            let image = image::load_from_memory(&response.unwrap().bytes).unwrap();
+            sender.send(image);
+        });
+        promise
+    });
+
+    Ok(result.block_until_ready().clone())
 }
 
 pub fn fetch_image_bytes(url: &str) -> Result<Vec<u8>, Box<dyn Error>> {
-	let mut promise: Option<Promise<Vec<u8>>> = None;
+    let mut promise: Option<Promise<Vec<u8>>> = None;
 
-	let result = promise.get_or_insert_with(|| {
-		let (sender, promise) = Promise::new();
-		let request = ehttp::Request::get(url);
-		ehttp::fetch(request, move |response| {
-			let bytes = response.unwrap().bytes.to_vec();
-			sender.send(bytes);
-		});
-		promise
-	});
-	
-	Ok(result.block_until_ready().clone())
+    let result = promise.get_or_insert_with(|| {
+        let (sender, promise) = Promise::new();
+        let request = ehttp::Request::get(url);
+        ehttp::fetch(request, move |response| {
+            let bytes = response.unwrap().bytes.to_vec();
+            sender.send(bytes);
+        });
+        promise
+    });
+
+    Ok(result.block_until_ready().clone())
 }
 
 pub fn show_sprite(sprite: &str, width: Option<u32>, height: Option<u32>, x: u16, y: i16) {
